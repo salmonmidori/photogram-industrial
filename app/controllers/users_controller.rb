431018210f82d9ef4e5691095402ba1790f73e16
Amy May Nguyen <anguye10@uchicago.edu>
2025-04-09 18:26:10 +0000

    Update how photos are shown

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
   def liked
     @user = User.find_by!(username: params.fetch(:username))
   end
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+  def discover
+    @user = User.find_by!(username: params.fetch(:username))
+  end
 end
